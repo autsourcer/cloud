05be7d20fe770b62536c60888344ec1189884670
autsourcer <umedamir@yandex.ru>
2020-02-19 10:47:24 +0100

    video

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-raspistill -n -t 1 -q 100 -dt -o image_%d.jpg -w 800 -h 600
+raspistill -n -t 1 -q 100 -dt -o image_%d.jpg -w 960 -h 720
 git add -A
 git commit -m "video"
 git push cloud master
